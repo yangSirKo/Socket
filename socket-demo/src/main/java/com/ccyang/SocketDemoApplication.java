e8652ef472973a337ff96eb8f0de22f6cd504a2d
yangjinpeng <yangjinpeng@duxiaoman.com>
2019-01-24 16:23:03 +0800

    init

---
+++ socket-demo/src/main/java/com/ccyang/SocketDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ccyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketDemoApplication.class, args);
+    }
+
+}
+
